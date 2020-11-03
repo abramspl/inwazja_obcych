f9bdd4eec8ec4b4c144e5029c90827ce89c5191d
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2020-11-03 21:06:53 +0100

    dostosowanie szybkosci statku

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Inicjalizacja statku kosmicznego i jego polozenie poczatkowe."""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Wczytanie obrazu statku kosmicznego i pobranie jego prostokata.
@@ -16,16 +17,24 @@ class Ship:
         # Kazdy nowy statek kosmiczny pojawiajacy sie na dole ekranu.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Polozenie poziome statku jest przechowywane w postaci liczby zmiennoprzecinkowej.
+        self.x = float(self.rect.x)
+
         # Opcje wskazujace na poruszanie sie statku.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Uaktualnienie polozenia statku na podstawie opcji wskazujacej na jego ruch."""
+
+        # Uaktualnienie wartosci wspolrzednej X statku, a nie jego prostokata.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Uaktualnienie obiektu rec na podstawie wartosci self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Wyswietlanie statku kosmicznego w jego aktualnym polozeniu."""
