98213bd80ecbe7a98b362b0682ae2ca6d87975f6
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2020-11-03 11:54:36 +0100

    utworzenie klasy statku kosmicznego

---
+++ ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship:
+    """Klasa przeznaczona do zarzadzania statkiem kosmicznym."""
+
+    def __init__(self,ai_game):
+        """Inicjalizacja statku kosmicznego i jego polozenie poczatkowe."""
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Wczytanie obrazu statku kosmicznego i pobranie jego prostokata.
+        self.image = pygame.image.load('images/ship.png')
+        self.rect = self.image.get_rect()
+
+        # Kazdy nowy statek kosmiczny pojawiajacy sie na dole ekranu
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Wyswietlanie statku kosmicznego w jego aktualnym polozeniu."""
+        self.screen.blit(self.image, self.rect)
