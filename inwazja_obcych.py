5126fa9034477467395385b3185139755f79e4e5
Łukasza Abramczyk <abramspl@users.noreply.github.com>
2020-11-04 14:47:16 +0100

    ograniczenie zsiegu poruszania sie statku po ekranie i dodanie trybu pelnoekranowego

--- inwazja_obcych.py
+++ inwazja_obcych.py
@@ -13,8 +13,15 @@ class InwazjaObcych:
         pygame.init()
         self.settings = Settings()
 
+        # Tryb gry onkienkowy
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Inwazja obcych')
+
+        # Tryb gry pelnoekranowy
+        # self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        # pygame.display.set_caption('Inwazja obcych')
 
         self.ship = Ship(self)
 
@@ -28,20 +35,32 @@ class InwazjaObcych:
 
     def _check_events(self):
         """Relacja na zdarzenia generowane przez klawiature i mysz."""
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        """Reakacja na nacisniecie klawisza."""
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self,event):
+        """Reakcja na zwolnienie klawisza."""
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Uaktualnienie obrazow na ekranie i przejscie do nowego ekranu."""
--- ship.py
+++ ship.py
@@ -28,9 +28,9 @@ class Ship:
         """Uaktualnienie polozenia statku na podstawie opcji wskazujacej na jego ruch."""
 
         # Uaktualnienie wartosci wspolrzednej X statku, a nie jego prostokata.
-        if self.moving_right:
+        if self.moving_right and self.rect.right <self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Uaktualnienie obiektu rec na podstawie wartosci self.x.
